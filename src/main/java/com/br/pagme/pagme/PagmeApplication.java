911232f23022a2f28df1cdf312e82337aeb830f9
Paulo Sérgio de Val Pereira Souza <pauser2200@gmail.com>
2025-02-16 13:51:46 -0300

    Add files via upload

---
+++ src/main/java/com/br/pagme/pagme/PagmeApplication.java
@@ -0,0 +1,13 @@
+package com.br.pagme.pagme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/pagme/pagme/PagmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pagme.pagme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
