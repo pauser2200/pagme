a58aab5c47bb434f1eeeab2f994175a41eb6fc27
Paulo <pauser2200@gmail.com>
2025-02-16 18:23:55 -0300

    feat:Ajustando .gitIgnore

--- src/main/java/com/br/pagme/divida/enums/TipoDivida.java
+++ src/main/java/com/br/pagme/divida/enums/TipoDivida.java
@@ -6,10 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum TipoDivida {
-    CREDITO("Crédito"),
-    EMPRESTIMO("Empréstimo"),
-    DEBITO("Débito"),
-    DINHEIRO("Dinheiro");
-
-    private String descricao;
+    CREDITO,
+    EMPRESTIMO,
+    DEBITO,
+    DINHEIRO;
 }
