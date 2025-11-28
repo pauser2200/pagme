d68a00a9c03435e0d630a5b560486916e244ef6d
Paulo <pauser2200@gmail.com>
2025-11-28 13:36:51 -0300

    Ajustando CORS de forma correta 2

--- backEnd/src/main/java/br/pagme/infra/security/WebConfiguration.java
+++ backEnd/src/main/java/br/pagme/infra/security/WebConfiguration.java
@@ -62,6 +62,7 @@ public class WebConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList(
+                "https://pagme-beta.vercel.app/pagme",
                 "https://pagme-beta.vercel.app",
                 "http://localhost:4200"
         ));
