35b1863020f1c2aaad687edbb30aecedc287fd22
Paulo <pauser2200@gmail.com>
2025-11-28 11:17:11 -0300

    Ajustando url de prod

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://URL-QUE-O-RENDER-VAI-TE-DAR' // Vamos preencher isso no Passo 2 abaixo
+  apiUrl: 'https://pagme-97cr.onrender.com'
 };
